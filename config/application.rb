265b1d158a2440f481122f7612aabdd4e557240e
s-satsangi <s.satsangi@gmail.com>
2021-05-18 23:33:56 -0400

    added dotenv gem

--- config/application.rb
+++ config/application.rb
@@ -22,12 +22,12 @@ Bundler.require(*Rails.groups)
 module Blog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.before_configuration do
-      env_file = File.join(Rails.root, 'config','local_env.yml')
-      YAML.load(File.open(env_file)).each do |key, value|
-        ENV[key.to_s] = value
-      end if File.exists?(env_file)
-    end
+    # config.before_configuration do
+    #   env_file = File.join(Rails.root, 'config','local_env.yml')
+    #   YAML.load(File.open(env_file)).each do |key, value|
+    #     ENV[key.to_s] = value
+    #   end if File.exists?(env_file)
+    # end
     config.load_defaults 6.1
     
     # Configuration for the application, engines, and railties goes here.
